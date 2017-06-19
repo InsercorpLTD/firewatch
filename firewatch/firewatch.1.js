c1096c26be2cb1c77760ec1ba8e11c4f54f546c7
leskjohnson <les@leskjohnson.com>
2017-06-19 17:08:57 -0400

    Update style and function

--- firewatch/firewatch.1.js
+++ firewatch/firewatch.1.js
@@ -4,6 +4,7 @@ let firewatch = {
 	'dweet': null, 
 	'ele': null,
 	'image': false,
+	'temp':'Fahrenheit',
 	'charts': [],
 	'data': [],
 	'gauges':[],
@@ -20,17 +21,35 @@ let firewatch = {
 	'setup':(function(){
 		firewatch.ele = document.getElementById(firewatch.id);
 		if (firewatch.ele){
-			firewatch.ele.innerHTML = '<div class="charts"></div><div class="image"></div>';
+			firewatch.ele.innerHTML = '<div class="charts"></div><div class="image"><a onclick="firewatch.camera_toggle();">Camera (click to start)</a></div>';
 			let ele = firewatch.ele.querySelector('.charts');
 			for (var i = 0; i < firewatch.gauges.length; i++){
-				let span = document.createElement("SPAN");
-				span.id = 'chart_'+i;
+				let div = document.createElement('DIV'),
+					span = document.createElement('SPAN'),
+					label = document.createElement('SPAN');
+
+				div.style = 'display:inline-block;text-align:center';
+				div.innerHTML = firewatch.gauges[i][0];
+
+				span.style = 'text-align:center';
+				span.id = 'chart_'+firewatch.gauges[i][0];
 				span.class = 'chart';
-				span.style = 'display:inline-block';
-				if(ele)ele.appendChild(span);
+				if (firewatch.gauges[i][0] == 'Temp')
+					span.onclick = firewatch.temp_toggle;
+
+				label.id = 'chart_label_'+firewatch.gauges[i][0];
+				label.innerHTML = firewatch.gauges[i][3];
+
+				if(ele){
+					div.appendChild(span);
+					div.appendChild(label);
+					ele.appendChild(div);
+				}
 				firewatch.gauges[i][1].width = firewatch.gauge.width;
 				firewatch.gauges[i][1].height = firewatch.gauge.height;
 				firewatch.gauges[i][1].minorTicks = firewatch.gauge.minorTicks;
+				firewatch.gauges[i][1].greenFrom = 0;
+				firewatch.gauges[i][1].greenTo = firewatch.gauges[i][1].max*.75;
 				firewatch.gauges[i][1].redFrom = firewatch.gauges[i][1].max*.9;
 				firewatch.gauges[i][1].redTo = firewatch.gauges[i][1].max;
 				firewatch.gauges[i][1].yellowFrom = firewatch.gauges[i][1].max*.75;
@@ -46,9 +65,9 @@ let firewatch = {
 		for (var i = 0; i < firewatch.gauges.length; i++) {
 			firewatch.data[i] = google.visualization.arrayToDataTable(
 				[['Label', 'Value'],
-				[firewatch.gauges[i][0],0]]
+				[firewatch.gauges[i][2],0]]
 			);
-			firewatch.charts[i] = new google.visualization.Gauge(document.getElementById('chart_'+i));
+			firewatch.charts[i] = new google.visualization.Gauge(document.getElementById('chart_'+firewatch.gauges[i][0]));
 			firewatch.charts[i].draw(firewatch.data[i], firewatch.gauges[i][1]);
 		}
 	}),
@@ -62,17 +81,43 @@ let firewatch = {
 				if(firewatch.data[i] && firewatch.charts[i]){
 					if(dweet.content[firewatch.gauges[i][0]]){
 						let lbl = firewatch.gauges[i][0],
-							num = dweet.content[lbl];
-				  		firewatch.data[i].setValue(0, 1, dweet.content[lbl]);
+							num = dweet.content[lbl],
+							label = firewatch.gauges[i][3] + '<br/>',
+							el = document.getElementById('chart_label_'+firewatch.gauges[i][0]);
+						if (lbl == 'Temp') {
+							if (firewatch.temp == 'Fahrenheit')
+								num = firewatch.fahrenheit(num);
+							label += num + ' ' + firewatch.temp;
+						} else label += num + ' ' + firewatch.gauges[i][2];
+				  		firewatch.data[i].setValue(0, 1, num);
 				  		firewatch.charts[i].draw(firewatch.data[i], firewatch.gauges[i][1]);
+						if (el) el.innerHTML = label
+
 					}
 				}
 			}
 			/* add the image stream to the div.image */
-			if (!firewatch.image && dweet.content.link){
-				let img = ele.querySelector('.image');
+			if (firewatch.image && dweet.content.link){
+				let img = firewatch.ele.querySelector('.image');
 				if (img) img.innerHTML = '<img src="'+dweet.content.link+'?action=stream" />';
-				firewatch.image = true;
+				firewatch.image = false;
+			}
+		}
+	}),
+	'fahrenheit':(function(degrees){ return Math.round(degrees * 9 / 5 + 32) }),
+	'camera_toggle':(function(e){
+		let img = firewatch.ele.querySelector('.image');
+		if (img) img.innerHTML = 'Loading...';
+		firewatch.image = true
+	}),
+	'temp_toggle':(function(e){
+		if (firewatch.temp == 'Fahrenheit')
+			firewatch.temp = 'Celsius';
+		else firewatch.temp = 'Fahrenheit';
+		for (var i = 0; i < firewatch.gauges.length; i++) {
+			if (firewatch.gauges[i][0] == 'Temp'){
+				firewatch.data[i].setValue(0,0,firewatch.temp);
+				break;
 			}
 		}
 	})
